37bae5e77cf6dcd27fe70fc463473e6010c36dd9
SinaSystem <sinasystem47@gmail.com>
2020-06-05 13:16:23 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyank/javatokotlindemo/models/Owner.kt
@@ -0,0 +1,4 @@
+package com.sriyank.javatokotlindemo.models
+
+
+data class Owner(var id: Int, var login: String?)
---
+++ app/src/main/java/com/sriyank/javatokotlindemo/models/SearchResponse.kt
@@ -0,0 +1,9 @@
+package com.sriyank.javatokotlindemo.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+	@SerializedName("total_count") var totalCount: Int,
+	var items: List<Repository>?
+)
